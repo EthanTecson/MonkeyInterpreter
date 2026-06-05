b872533b37875e5d0331d23a3b5ba23893c871ab
ethan <ethantecson@gmail.com>
2026-06-05 15:39:09 +0700

    40. Tests not passing as they shouldnt be

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -42,5 +42,5 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressionNode()
+func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal } 
