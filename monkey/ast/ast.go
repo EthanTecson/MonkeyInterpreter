01aa2e7d8c3fd2d5c4b40e771604e312f22929af
ethan <ethantecson@gmail.com>
2026-07-10 15:27:42 -0400

    top of 51. Added in String() method from Node interface for LetStatement, ExpressionStatement, Identifier, and ReturnStatement.

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -8,11 +8,13 @@
 package ast
 
 import (
+	"bytes"
 	"monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,13 +31,26 @@ type Program struct {
 	Statements []Statement
 }
 
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
 type ReturnStatement struct {
 	Token token.Token // the 'return' token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
-func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal}
+type LetStatement struct {
+	Token token.Token // The token.LET
+	Name *Identifier
+	Value Expression
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
@@ -45,19 +60,68 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-type LetStatement struct {
-	Token token.Token // The token.LET
-	Name *Identifier
-	Value Expression
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) expressionNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	// TODO: nil statement will be taken out when we can parse expression
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	// TODO: nil statement will be taken out when we can parse expression
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-type Identifier struct {
-	Token token.Token // the token.IDENT token
-	Value string
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	// TODO: nil statement will be taken out when we can parse expression
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
+
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string { return i.Value }
